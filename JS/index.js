0fab56f417090e2fa856352008eece2fd0c6d707
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-10-03 20:54:32 +0300

    adding some JS

--- JS/index.js
+++ JS/index.js
@@ -5,13 +5,15 @@ let infoBoxQuitBtn =infoBox.querySelector(".buttons .quit");
 let infoBoxContinueBtn =infoBox.querySelector(".buttons .Continue");
 let quizBox =document.querySelector(".quiz-box");
 let timerSec=quizBox.querySelector("header .timer .timer-sec");
-let questionsText =quizBox.querySelector("section .questions-text span");
-let options =quizBox.querySelectorAll ("section .option-list .option");
+let questionsText =quizBox.querySelector("section .questions-text");
+let options =quizBox.querySelector ("section .option-list");
 let optionIcon =quizBox.querySelectorAll ("section .option-list .option .icon");
-let nextBtn =quizBox.querySelector ("footer .next-btn button");
-let QueNumber =quizBox.querySelector (" footer .total-questions span p");
+let nextBtn =quizBox.querySelector ("footer .next-btn");
+let QueNumber =quizBox.querySelector (" footer .total-questions");
 let resultBox =document.querySelector (".result-box");
 let score = resultBox.querySelector("score-text span p");
+let crossIcon ='<div class="icon cross"><i class="fas fa-times"></i></div>';
+let tickIcon ='<div class="icon tick"><i class="fas fa-check"></i></div>';
 // start and Quit Quiz :
 startBtn.onclick = () => {
     infoBox.classList.add("active");
@@ -19,8 +21,53 @@ startBtn.onclick = () => {
 infoBoxQuitBtn.onclick = () => {
     infoBox.classList.remove("active");
 }
+let queCounter = 0 ;
 // start quiz :
 infoBoxContinueBtn.onclick =() => {
     infoBox.classList.remove("active");
     quizBox.classList.add("active");
+    showQuistions(queCounter);
+}
+// next button :
+nextBtn.onclick = () => {
+    if (queCounter<questions.length-1){
+    queCounter++;
+    showQuistions(queCounter);
+    }
+}
+function showQuistions(index) {
+    let quistionTag ="<span>"+questions[index].num + " ."+ questions[index].ques +"</span>";
+    let optionTag ="<div class='option'><span>"+ questions[index].quesOptions[0] +"</span></div>"
+                 + "<div class='option'><span>"+ questions[index].quesOptions[1] +"</span></div>"
+                 + "<div class='option'><span>"+ questions[index].quesOptions[2] +"</span></div>"
+                 + "<div class='option'><span>"+ questions[index].quesOptions[3] +"</span></div>";
+    let quistionNumberTag = "<span>"+questions[index].num + " of "+ questions.length + " questions" +"</span>";
+    questionsText.innerHTML = quistionTag ;
+    options.innerHTML = optionTag ;
+    QueNumber.innerHTML =quistionNumberTag;
+    // adding atttibute to all option :
+    let opt = options.querySelectorAll(".option");
+    for (let i=0 ;i<opt.length; i++) {
+        opt[i].setAttribute("onclick","optionSellected(this)");
+    }
+}
+function optionSellected (answer) {
+    let uerAns = answer.textContent;
+    let correctAns = questions[queCounter].answer;
+    if (uerAns===correctAns){
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend",tickIcon);
+    }else {
+        answer.classList.add("wrong");
+        answer.insertAdjacentHTML("beforeend",crossIcon);
+    }
+    // once user selescted disabled all options and  show the right answer :
+    let opt = options.querySelectorAll(".option");
+    for (let i=0 ; i<opt.length ; i++) {
+        opt[i].classList.add("disabled");
+        if (opt[i].textContent == correctAns && uerAns!==correctAns ){
+            opt[i].classList.add("correct");
+            opt[i].insertAdjacentHTML("beforeend",tickIcon);
+        }
+    }
 }
---
+++ JS/questions.js
@@ -0,0 +1,113 @@
+// array of questions 
+let questions = [
+    {
+    num : 1,
+    ques : "There are ___ levels of heading in HTML" ,
+    answer : "Six" ,
+    quesOptions:[
+        "Three",
+        "Four",
+        "Five",
+        "Six"
+    ]
+    },
+    {
+    num : 2,
+    ques : "The purpose of markup is to" ,
+    answer : "both A & B" ,
+    quesOptions:[
+        "add hypertext capabilities",
+        "enhance the document",
+        "both A & B",
+        "none of the above"
+    ]
+    },
+    {
+    num : 3,
+    ques : "For a particular font its size attribute can be an absolute value ranging form" ,
+    answer : "1-10" ,
+    quesOptions:[
+        "1-10",
+        "1-9",
+        "1-8",
+        "1-7"
+    ]
+    },
+    {
+    num : 4,
+    ques : "The Major components of the Web browser are ___" ,
+    answer : " All Of the Above." ,
+    quesOptions:[
+        "Menu Bar.",
+        "ToolBar.",
+        "Location",
+        " All Of the Above."
+    ]
+    },
+    {
+    num : 5,
+    ques : "Example of picture editing software is" ,
+    answer : "photoshop" ,
+    quesOptions:[
+        "ms paint",
+        "photoshop",
+        "animator pro",
+        "none of the above"
+    ]
+    },
+    {
+    num : 6,
+    ques : "CD ROM stands for" ,
+    answer : "compact disk read only memory" ,
+    quesOptions:[
+        "computer disk read only memory",
+        "computer disk read out memory",
+        "compact disk read only memory",
+        "compact disk read out memory"
+    ]
+    },
+    {
+    num : 7,
+    ques : "The decimal range of network address for class b network is between" ,
+    answer : "128-191" ,
+    quesOptions:[
+        "127-190",
+        "128-190",
+        "128-191",
+        "127-255"
+    ]
+    },
+    {
+    num : 8,
+    ques : "CGI stand for",
+    answer : "Common Gateway Interface" ,
+    quesOptions:[
+        "Command Gate Interface",
+        "Common Gateway Interface",
+        "Common Gate input",
+        "None of the above"
+    ]
+    },
+    {
+    num : 9,
+    ques : "Example for bitmap application is" ,
+    answer : "MS paint" ,
+    quesOptions:[
+        "MS paint",
+        "Photoshop",
+        "Sound recorder",
+        "None of the above"
+    ]
+    },
+    {
+    num : 10,
+    ques : "The ideal rate for animation is" ,
+    answer : "30 Frames/second" ,
+    quesOptions:[
+        "30 Frames/second",
+        "20 Frames/second",
+        "40 Frames/second",
+        "none of the above"
+    ]
+    },
+];
